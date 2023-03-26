847eca033150198fad316d764ffd7d697aed71c3
Matteo <105592696+Howard3798@users.noreply.github.com>
2023-03-26 20:23:15 +0200

    Modificato i file:

--- Matteo.Excersize/Es22.03.Banca/Program.cs
+++ Matteo.Excersize/Es22.03.Banca/Program.cs
@@ -6,27 +6,8 @@ namespace Es22._03.Banca
     {
         static void Main(string[] args)
         {
-            IswiftSystem Iecb = new SwiftCentralBank("European Central Bank", "Strabourg, Francia");
+            SwiftCentralBank ecb = new SwiftCentralBank("European Central Bank", "Strabourg, Francia");
             CentralBank rcb = new CentralBank("Russian Central Bank", "Moscow, Russia");
-            CentralBank rcb2 = new CentralBank("Russian Central Bank", "Moscow, Russia");
-
-
-            SwiftCentralBank ecb = (SwiftCentralBank)Iecb;
-            SwiftCentralBank ecb2 = (SwiftCentralBank)Iecb;
-
-            ecb.createCommercialBankItalian("Intesa Sanpaolo", "Turin");
-            ecb2.createCommercialBankItalian("Unicredit", "Turin");
-            rcb.createCommercialBankRussian("VTB Bank", "Moscow");
-            rcb2.createCommercialBankRussian("Sperbank", "Moscow");
-
-            ecb.commercialBankItalian.CreateAccount("Matteo", "Luccisano", 123456);
-            ecb2.commercialBankItalian.CreateAccount("Alvaro", "Potamen", 0000000);
-            rcb.commercialBankRussian.CreateAccount("Vladimir", "Putin", 654321);
-            rcb2.commercialBankRussian.CreateAccount("Svletana", "Larukovic", 6539283);
-
-            ecb.transferMoney(ecb.commercialBankItalian, ecb2.commercialBankItalian);
-            ecb.transferMoney(ecb.commercialBankItalian, rcb2.commercialBankRussian);
-            rcb.transferMoney(rcb.commercialBankRussian, rcb2.commercialBankRussian);
 
         }
     }
