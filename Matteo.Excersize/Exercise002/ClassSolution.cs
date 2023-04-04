0c29797e2f602de148b44a4370f43c7f72ce9e1d
Matteo <105592696+Howard3798@users.noreply.github.com>
2023-04-04 16:37:38 +0200

    Modifica OOP.Excersice.Polimorfismo
    Usate le interfacce

---
+++ Matteo.Excersize/Exercise002/ClassSolution.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OOP.Exercise.Polimorfismo.v002
+{
+    internal class ClassSolution
+    {
+        #region
+       /* public abstract class IntermdiarioFinanziario
+        {
+            public string Name { get; set; }
+
+            protected virtual Asset BuyAsset(IntermdiarioFinanziario intermdiarioFinanziario)
+            {
+                return intermdiarioFinanziario.BuyAsset(intermdiarioFinanziario);
+            }
+            public class Asset
+            {
+
+            }
+        }
+        public class CryptoIntermediary : IntermdiarioFinanziario
+        {
+            protected override Asset BuyAsset(IntermdiarioFinanziario intermdiarioFinanziario)
+            {
+                return null;
+            }
+        }
+        public class StockIntermediary : IntermdiarioFinanziario
+        {
+            public StockIntermediary()
+            {
+
+            }
+            protected override Asset BuyAsset(IntermdiarioFinanziario intermdiarioFinanziario)
+            {
+                Console.WriteLine($"I'm Buying Stocks!");
+                return null;
+            }
+
+        }
+        public class CrytoCEX : CryptoIntermediary
+        {
+            public CrytoCEX() : base()
+            {
+
+            }
+            protected override Asset BuyAsset(IntermdiarioFinanziario intermdiarioFinanziario)
+            {
+                Console.WriteLine($"I'm Buying Crypto from {this.Name}");
+                return null;
+
+            }
+        }
+        public class StockMarket : StockIntermediary
+        {
+            protected override Asset BuyAsset(IntermdiarioFinanziario intermdiarioFinanziario)
+            {
+                Console.WriteLine($"I'm Buying Stocks from {this.Name}");
+                return null;
+            }
+        }
+        public class Banca : IntermdiarioFinanziario
+        {
+            public void BuyCrypto(IntermdiarioFinanziario intermdiarioFinanziario)
+            {
+                base.BuyAsset(intermdiarioFinanziario);
+            }
+            public void BuyStocks(IntermdiarioFinanziario intermdiarioFinanziario)
+            {
+                base.BuyAsset(intermdiarioFinanziario);
+            }
+        }*/
+        #endregion
+
+
+       private interface IcryptoIntermediary
+        {
+            public void buyCrypto();
+        }
+        private interface IstockIntermediary
+        {
+            public void buyStock();
+        }
+        public class StockMarket : IstockIntermediary
+        {
+            void IstockIntermediary.buyStock()
+            {
+                Console.WriteLine("Stocks");
+            }
+        }
+
+        public class CryptoExchange : IcryptoIntermediary
+        {
+            void IcryptoIntermediary.buyCrypto()
+            {
+                Console.WriteLine("Crypto");
+            }
+        }
+
+        public class bank: IcryptoIntermediary, IstockIntermediary
+        {
+            IstockIntermediary stockMarket = new StockMarket();
+            IcryptoIntermediary cryptoExchange = new CryptoExchange();
+
+            
+            public void buyCrypto()
+            {
+                cryptoExchange.buyCrypto();                
+            }
+
+            public void buyStock()
+            {                
+                stockMarket.buyStock();
+            }
+        }
+    }
+}
---
+++ Matteo.Excersize/Exercise002/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using static OOP.Exercise.Polimorfismo.v002.ClassSolution;
+
+namespace Tests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            ////vedere lo stockMarket/Cryptoexchange dall'esterno
+            //CrytoCEX binance = new CrytoCEX() { Name = "Binance" }; // Crypto Exchange 
+            //StockMarket pizzaAffari = new StockMarket() { Name = "pizzaAffari" };// StockMarket -> Borsa
+
+
+            // Banca _banca = new Banca();
+            //_banca.BuyCrypto(binance);
+            //_banca.BuyStocks(pizzaAffari);
+            //MyCryptoMarket myCrypto = new MyCryptoMarket();
+            //MyCommertialBank myCommertialBank = new MyCommertialBank();
+            //myCommertialBank.BuyCrypto(myCrypto);
+            #endregion
+            
+            bank banca = new bank();
+
+            banca.buyCrypto();
+            banca.buyStock();
+
+            
+
+            
+
+            
+
+            
+
+            
+
+            
+            
+
+            
+
+            
+        }
+    }
+
+
+
+
+
+}
+
