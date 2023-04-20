4e46212ca58d5fb2590546084c87478eb7e79904
Matteo <105592696+Howard3798@users.noreply.github.com>
2023-04-20 13:05:01 +0200

    Inserito il backgorund color nel costruttore netflix
    Modificato il nome da Listsong a Listmultimedia
    
    Dichiarato la variabile multimedia, che quando viene creato il costruttore Spotify è uguale a song, netflix è film (una modifica visiva all'utente)

---
+++ Matteo.Excersize/SongStreaming/IPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongStreaming
+{
+    interface IPlayer
+    {
+        public void Play(int SongId);
+        public void ShowPlaying();
+        public void StartPlaying();
+        public void Stop();
+        public void Pause();
+        public void Rate();
+        public void Forward();
+        public void Backward();
+        public void ListMultimedia();
+
+    }
+}
---
+++ Matteo.Excersize/SongStreaming/Netflix.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongStreaming
+{
+    public sealed class Netflix : StreamingPlatform
+    {
+        public Netflix()
+        {
+            files = new NetflixVideo[] {
+                new NetflixVideo(){ title ="Titanic",Color = System.ConsoleColor.Blue},
+                new NetflixVideo(){ title ="Terminator", Color = System.ConsoleColor.Cyan},
+                new NetflixVideo(){ title ="Alien", Color = System.ConsoleColor.Green},
+                new NetflixVideo(){ title ="Avatar", Color = System.ConsoleColor.Yellow}
+
+                };
+
+            totalTacks = files.Length;
+            multimedia = "film";
+        }
+        private class NetflixVideo : Video
+        {
+            public NetflixVideo()
+            {
+            }
+        }
+    }
+}
