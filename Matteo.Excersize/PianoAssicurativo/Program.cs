c35e19f55d3d7423a9baa2540db51bc753fb283f
Matteo <105592696+Howard3798@users.noreply.github.com>
2023-04-28 14:53:37 +0200

    Esercizio PianoAssicurativo

--- Matteo.Excersize/PianoAssicurativo/Program.cs
+++ Matteo.Excersize/PianoAssicurativo/Program.cs
@@ -14,7 +14,7 @@ namespace PianoAssicurativo
             Intermediary.request();
             var response = Console.ReadLine();
 
-            if (response.ToUpper() == "Y") Intermediary.confirm(assicurazione, euDigitalWallet.Clinical);
+            if (response.ToUpper() == "Y") Console.WriteLine(Intermediary.confirm(assicurazione, euDigitalWallet.Clinical));
             else Console.WriteLine("Non hai consentito l'accesso ai tuoi dati e non puoi creare un piano assicurativo");
 
 
@@ -26,6 +26,10 @@ namespace PianoAssicurativo
             string _name;
             string _surname;
             string _cf;
+
+            public string Name { get => _name; }
+            public string Surname { get => _surname; }
+            public string cf { get => _cf; }
 
             public Client(string name, string surname, string cf)
             {
@@ -53,8 +57,10 @@ namespace PianoAssicurativo
         public class EuDigitalWallet : Intermediary
         {
             clinicalSituation _clinical;
-            public clinicalSituation Clinical { get => _clinical; }
             Client _client;
+            public clinicalSituation Clinical { get => _clinical; }
+            public Client client { get => _client; }
+            
             public EuDigitalWallet(Client client, string datisanitari)
             {
                 _client = client;
@@ -68,12 +74,12 @@ namespace PianoAssicurativo
         {
             public class clinicalSituation
             {
-
                 string _datisanitari;
                 public string Datisanitari { get => _datisanitari; }
                 public clinicalSituation(string datisanitari)
                 {
                     _datisanitari = datisanitari;
+                    
                 }
             }
             public static void request()
@@ -81,12 +87,12 @@ namespace PianoAssicurativo
                 Console.WriteLine("Per creare un nuovo piano assicurativo, devi consentire l'accesso ai tuoi dati sanitari.\nPremere Y per consentire, N per declinare.");
             }
 
-            public static void confirm(Assicurazione assicurazione, clinicalSituation datisanitari)
+            public static string confirm(Assicurazione assicurazione, clinicalSituation datisanitari)
             {
 
                 Console.WriteLine("Ho accesso ai dati sanitari dell'utente");
                 Thread.Sleep(1000);
-                Console.WriteLine(string.Format(assicurazione.createPlan(datisanitari)));
+                return string.Format(assicurazione.createPlan(datisanitari));
             }
         }
     }
